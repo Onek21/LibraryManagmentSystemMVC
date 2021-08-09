0e9a674963b83b256121f4c564927bbfe6247dce
Szymon Kląskała <klaskala.sz@gmail.com>
2021-08-09 12:09:49 +0200

    add scaffolding identity

---
+++ LibraryManagmentSystemMVC.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LibraryManagmentSystemMVC.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LibraryManagmentSystemMVC.Web.Areas.Identity.IdentityHostingStartup))]
+namespace LibraryManagmentSystemMVC.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
