8b03995bcd150d0a627371a341d747d5f5cdfe65
Szymon Kląskała <klaskala.sz@gmail.com>
2021-08-12 14:56:16 +0200

    create api login

---
+++ LibraryManagmentSystemMVC.Api/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using LibraryManagmentSystemMVC.Domain.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentSystemMVC.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _singInManager;
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config, SignInManager<ApplicationUser> signInManager)
+        {
+            _singInManager = signInManager;
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserModel loginModel)
+        {
+            IActionResult respone = Unauthorized();
+            var success = AuthenticateUser(loginModel);
+
+            if(success)
+            {
+                var tokenString = GenerateJsonWebToken(loginModel);
+                respone = Ok(new { token = tokenString });
+            }
+            return respone;
+        }
+
+        private object GenerateJsonWebToken(UserModel loginModel)
+        {
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(120), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private bool AuthenticateUser(UserModel loginModel)
+        {
+            var result = _singInManager.PasswordSignInAsync(loginModel.Name, loginModel.Password, true, lockoutOnFailure: false).Result;
+
+            return result.Succeeded;
+        }
+    }
+}
